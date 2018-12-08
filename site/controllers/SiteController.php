ae9c57c9a960e256401a315ff7264b768db99a2b
xushenkai <xushenkai@classba.cn>
2018-12-08 19:52:42 +0800

    test

--- site/controllers/SiteController.php
+++ site/controllers/SiteController.php
@@ -1,68 +1,27 @@
 <?php
 namespace site\controllers;
 
+
 use Yii;
-use yii\base\InvalidParamException;
-use yii\web\BadRequestHttpException;
-use yii\web\Controller;
-use yii\filters\VerbFilter;
-use yii\filters\AccessControl;
 use common\models\LoginForm;
 use frontend\models\PasswordResetRequestForm;
 use frontend\models\ResetPasswordForm;
 use frontend\models\SignupForm;
 use frontend\models\ContactForm;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * Site controller
  */
 class SiteController extends Controller
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout', 'signup'],
-                'rules' => [
-                    [
-                        'actions' => ['signup'],
-                        'allow' => true,
-                        'roles' => ['?'],
-                    ],
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
+    public function init(){
+        parent::init();
     }
     public function checkField($data = '', $default = ''){
 
@@ -263,7 +222,7 @@ class SiteController extends Controller
         json_decode($string);
         return (json_last_error() == JSON_ERROR_NONE);
     }
-    /**
+        /**
      * @desc 处理 body 参数
      */
     public function BodyParamsHandle($data)
@@ -339,5 +298,4 @@ class SiteController extends Controller
         }
         echo json_encode($children);exit;
     }
-
 }
